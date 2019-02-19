6e1d4fc0e54fc679542b5955ba04606d4e576eac
Ambulancia <>
2019-02-18 23:35:32 -0300

    Added new command: STORE CHARACTER CODE OF str IN num

--- src/nvm.h
+++ src/nvm.h
@@ -742,6 +742,21 @@ void execute(vector<string> & lines)
 	    s[1] = '\0';
             vm_stack.push(alfanum(string(&s[0])));
 	}
+	//Character to number
+	else if(token == "ORD"){
+            check_stack_size(1);
+            alfanum a = vm_stack.top();
+            vm_stack.pop();
+            string chr = a.txt_value();
+            if (chr.size() != 1) {
+                cerr << "\033[1;31mError: ORD expects a string of length 1, got: \033[1;37m";
+                cerr << chr << endl;
+                cerr << "\033[0m" << endl;
+                exit(1);
+	    }
+	    int ord = chr[0];
+            vm_stack.push(ord);
+	}
         // - Discard top of stack -
         else if(token == "BEEP"){
           system("beep");
