e5e29e4fcac324cef0c15f3ef087df39654f9a61
Victor Scott Cushman <VictorSCushman@gmail.com>
2019-03-16 14:03:11 -0400

    Whitespace adjustments
    The .h and .cpp files contained a mix of tab and space indentation.
    These, in addition to the makefile, also contained end-of-line
    whitespace.
    This commit adjusts this whitespace so that it is consistent across all
    source files.

--- src/ldpl.h
+++ src/ldpl.h
@@ -16,7 +16,7 @@ struct compiler_state{
     unsigned int section_state = 0; //0 no section, 1 data, 2 procedure
     unsigned int current_line = 0;
     bool in_subroutine = false;
-	string current_file = "";
+    string current_file = "";
     //Code to output (plain C code)
     vector<string> variable_code;
     vector<string> output_code;
@@ -59,8 +59,8 @@ void tokenize(string & line, unsigned int line_num, vector<string> & tokens, str
 void split_vector(string & line, queue<string> & tokens);
 void compile_line(vector<string> & tokens, unsigned int line_num, compiler_state & state);
 bool line_like(string model_line, vector<string> & tokens, compiler_state & state); //Important to pass tokens by copy
-bool is_number (string number);
-bool is_natural (string number);
+bool is_number(string number);
+bool is_natural(string number);
 bool is_string(string & token);
 bool is_vector_index(string & token);
 bool is_num_var(string & token, compiler_state & state);
