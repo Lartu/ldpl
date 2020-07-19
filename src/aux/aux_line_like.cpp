4c9796d510591671180e8ce9b94806bce0733107
Mike Redko <mike.redko49@gmail.com>
2020-07-19 14:34:39 +0300

    replace “ if(!X) return false “ with “ return X “

--- src/aux/aux_line_like.cpp
+++ src/aux/aux_line_like.cpp
@@ -70,84 +70,68 @@ bool line_like(string model_line, vector<string> &tokens, compiler_state &state)
         }
         else if (model_tokens[i] == "$num-vec") // $num-vec is NUMBER vector
         {
-            if (!is_num_map(tokens[j], state))
-                return false;
+            return is_num_map(tokens[j], state);
         }
         else if (model_tokens[i] == "$str-vec") // $str-vec is TEXT vector
         {
-            if (!is_txt_map(tokens[j], state))
-                return false;
+            return is_txt_map(tokens[j], state);
         }
         else if (model_tokens[i] == "$list")
         { // $list is a LIST
-            if (variable_type(tokens[j], state).back() != 3)
-                return false;
+            return variable_type(tokens[j], state).back() == 3;
         }
         else if (model_tokens[i] == "$map")
         { // $map is a MAP
-            if (variable_type(tokens[j], state).back() != 4)
-                return false;
+            return variable_type(tokens[j], state).back() == 4;
         }
         else if (model_tokens[i] == "$scalar-list")
         { // $scalar-list is a LIST of scalar values
-            if (!is_scalar_list(tokens[j], state))
-                return false;
+            return is_scalar_list(tokens[j], state);
         }
         else if (model_tokens[i] == "$num-list") // $num-vec is NUMBER list
         {
-            if (!is_num_list(tokens[j], state))
-                return false;
+            return is_num_list(tokens[j], state);
         }
         else if (model_tokens[i] == "$str-list") // $str-vec is TEXT list
         {
-            if (!is_txt_list(tokens[j], state))
-                return false;
+            return is_txt_list(tokens[j], state);
         }
         else if (model_tokens[i] == "$list-list") // $str-vec is a LIST of LISTs
         {
-            if (!is_list_list(tokens[j], state))
-                return false;
+            return is_list_list(tokens[j], state);
         }
         else if (model_tokens[i] == "$map-list") // $str-vec is LIST of MAPs
         {
-            if (!is_map_list(tokens[j], state))
-                return false;
+            return is_map_list(tokens[j], state);
         }
         else if (model_tokens[i] == "$collection") // $collection is either a MAP or a LIST
         {
             // if(!is_scalar_map(tokens[j], state) && !is_scalar_list(tokens[j], state) && (variable_type(tokens[j], state).size() < 2)) return false;
-            if (variable_type(tokens[j], state).size() < 2)
-                return false;
+            return variable_type(tokens[j], state).size() >= 2
         }
         else if (model_tokens[i] == "$literal") // $literal is either a NUMBER or a TEXT
         {
-            if (!is_string(tokens[j]) && !is_number(tokens[j]))
-                return false;
+            return is_string(tokens[j]) && is_number(tokens[j]);
         }
         else if (model_tokens[i] == "$string") // $string is a TEXT literal
         {
-            if (!is_string(tokens[j]))
-                return false;
+            return is_string(tokens[j]);
         }
         else if (model_tokens[i] == "$number") // $number is a NUMBER literal
         {
-            if (!is_number(tokens[j]))
-                return false;
+            return is_number(tokens[j]);
         }
         else if (model_tokens[i] == "$expression") // $expression is NUMBER, TEXT, TEXT-VAR, NUMBER-VAR
         {
-            if (!is_expression(tokens[j], state))
-                return false;
+            return is_expression(tokens[j], state);
         }
         else if (model_tokens[i] == "$str-expr") // $str-expr is either a TEXT or a TEXT variable
         {
-            if (!is_txt_expr(tokens[j], state))
-                return false;
+            return is_txt_expr(tokens[j], state);
         }
         else if (model_tokens[i] == "$num-expr") // $num-expr is either a NUMBER or a NUMBER variable
         {
-            if (!is_num_expr(tokens[j], state))
-                return false;
+            return is_num_expr(tokens[j], state);
         }
         else if (model_tokens[i].find("$var-type-") == 0) // variable with a given type number
         {
@@ -157,27 +141,23 @@ bool line_like(string model_line, vector<string> &tokens, compiler_state &state)
                 return false;
             for (size_t t = 0; t < actual_type.size(); ++t)
             {
-                if ((int)actual_type[t] != expected_type[t] - '0')
-                    return false;
+                return (int)actual_type[t] == expected_type[t] - '0';
             }
         }
         else if (model_tokens[i] == "$natural") // $natural is an integer greater than 0
         {
-            if (!is_natural(tokens[j]))
-                return false;
+            return is_natural(tokens[j]);
         }
         else if (model_tokens[i] == "$display") // multiple NUMBER, TEXT, TEXT-VAR, NUMBER-VAR
         {
             for (; j < tokens.size(); ++j)
             {
-                if (!is_expression(tokens[j], state))
-                    return false;
+                return is_expression(tokens[j], state);
             }
         }
         else if (model_tokens[i] == "$subprocedure") // $subprocedure is a SUB-PROCEDURE
         {
-            if (!is_subprocedure(tokens[j], state))
-                return false;
+            return is_subprocedure(tokens[j], state);
         }
         else if (model_tokens[i] == "$external") // $external is a C++ function defined elsewhere
         {
